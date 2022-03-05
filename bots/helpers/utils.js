4e87816d121b60bbdb0059ea00f9c6492c35b6c9
Davy Jones <Hello@Galleon.community>
2022-03-05 22:49:56 +0000

    discord bots

---
+++ bots/helpers/utils.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch')
+
+module.exports.fetchData = async () => {
+  try {
+    const tokenData = await (
+      await fetch(
+        `https://api.coingecko.com/api/v3/coins/${process.env.TOKEN_ID}`,
+      )
+    ).json()
+
+    const price = tokenData.market_data.current_price.usd
+    const symbol = tokenData.symbol.toUpperCase()
+    const circSupply = tokenData.market_data.circulating_supply
+    const change = tokenData.market_data.price_change_percentage_24h
+
+    return { price, symbol, circSupply, change }
+  } catch (err) {
+    console.log(err)
+    return undefined
+  }
+}
+
+module.exports.numberWithCommas = (num) => num.toLocaleString()

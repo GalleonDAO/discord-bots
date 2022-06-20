9c6e7f6e489d3455c6fc7b16f699a3994066c77d
crown_dev <crown@galleon.community>
2022-06-20 16:57:21 +0100

    Move help messages to embeds and fix replies

--- powdermonkey/src/utils/discordInteractionHandler.js
+++ powdermonkey/src/utils/discordInteractionHandler.js
@@ -1,5 +1,12 @@
-const { LOG_SEVERITY } = require('../services/azureLoggingService');
+const { LOG_SEVERITY } = require("../services/azureLoggingService");
+const { ServiceContainer } = require("../services/serviceContainer");
+
+/**
+ * @type {import('./embedBuilder').EmbedBuilder}
+ */
+const embedBuilder = ServiceContainer.getInstance().getService("embedBuilder");
 const emoji = "<a:dancing_crab:960471758954192907>";
+
 /**
  * @typedef { Interaction, InteractionReplyOptions } = require("discord.js");
  */
@@ -8,113 +15,150 @@ const emoji = "<a:dancing_crab:960471758954192907>";
  * This Handler is used for global error handling and decoration of discord interactions
  * Making the interaction private prevents bypassing global acitons
  */
-class DiscordInteractionHandler{
-    #interaction;
-    #logger;
+class DiscordInteractionHandler {
+  #interaction;
+  #logger;
 
-    /**
-     * This Handler is used for global error handling and decoration of discord interactions
-     * Making the interaction private prevents bypassing global acitons
-     * @param {import('discord.js').Interaction } interaction
-     * @param {import('../utils/logWrapper.js').LogWrapper} logger
-     */
-    constructor(interaction, logger){
-        this.#interaction = interaction;
-        this.#logger = logger;
+  /**
+   * This Handler is used for global error handling and decoration of discord interactions
+   * Making the interaction private prevents bypassing global acitons
+   * @param {import('discord.js').Interaction } interaction
+   * @param {import('../utils/logWrapper.js').LogWrapper} logger
+   */
+  constructor(interaction, logger) {
+    this.#interaction = interaction;
+    this.#logger = logger;
+  }
+
+  /**
+   * @param {import('discord.js').InteractionReplyOptions } message
+   */
+  async reply(message, commandName) {
+    this.#interaction
+      .reply(message)
+      .then(() =>
+        this.send(
+          embedBuilder.createMessageEmbed(
+            `Another sailor armed with the /${commandName} command!`,
+            `${emoji} Time to fetch more powder ${emoji}.`,
+            "monkey.png",
+            false
+          ),
+          this.#interaction.channel
+        )
+      )
+      .catch((err) => this.genericError(this.reply.name, err));
+  }
+
+  /**
+   * @param {import('discord.js').InteractionReplyOptions } message
+   */
+  async followUp(message, parent) {
+    try {
+      if (parent) await parent.reply(message);
+      else await this.#interaction.followUp(message);
+    } catch (err) {
+      await this.genericError(this.followUp.name, err);
     }
+  }
 
-    /**
-     * @param {import('discord.js').InteractionReplyOptions } message 
-     */
-    async reply(message, commandName){
-        try{
-            await this.#interaction.reply(message);
-            await this.followUp(
-                `Another sailor armed with the /${commandName} command!\r\n${emoji} Time to fetch more powder ${emoji}.`);
-        }
-        catch(err){
-            await this.genericError(this.reply.name, err);
-        }
+  /**
+   * @param {import('discord.js').InteractionReplyOptions } message
+   */
+  async send(message, channel) {
+    try {
+      await channel.send(message);
+    } catch (err) {
+      await this.genericError(this.followUp.name, err);
     }
+  }
 
-    /**
-     * @param {import('discord.js').InteractionReplyOptions } message 
-     */
-    async followUp(message){
-        try{
-            await this.#interaction.followUp(message);
-        }
-        catch(err){
-            await this.genericError(this.followUp.name, err);
-        }
+  /**
+   * @returns {String} The name of the Subcommand Group
+   */
+  getSubcommandGroup() {
+    return this.#interaction.options.getSubcommandGroup();
+  }
+
+  /**
+   * @returns {String} The name of the subcommand
+   */
+  getSubcommand() {
+    return this.#interaction.options.getSubcommand();
+  }
+
+  /**
+   * @param {String} key The Name of the command option
+   * @returns {?String} The value of the option if exists
+   */
+  getStringChoice(key) {
+    return this.#interaction.options.getString(key);
+  }
+
+  /**
+   * @param {Object} error
+   */
+  async genericError(functionName, error) {
+    this.#logger.logMessage(
+      LOG_SEVERITY.ERROR,
+      functionName,
+      error.stack,
+      error.message
+    );
+    try {
+      await this.#interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } catch {
+      // If the interaction token has expired sending an error reply will cause a fatal error
+      // No need to log this as the first catch will log error
     }
+  }
 
-    /**
-     * @returns {String} The name of the Subcommand Group
-     */
-    getSubcommandGroup(){
-        return this.#interaction.options.getSubcommandGroup();
+  /**
+   * @param {String} commandName
+   */
+  async commandNotExistsError(commandName) {
+    this.#logger.logMessage(
+      LOG_SEVERITY.WARN,
+      commandName,
+      "N/A",
+      `ERROR: Requested command: ${commandName} does not exist`
+    );
+    try {
+      await this.#interaction.reply({
+        content: `The specified command '${commandName}' does not exist`,
+        ephemeral: true,
+      });
+    } catch {
+      // If the interaction token has expired sending an error reply will cause a fatal error
+      // No need to log this as the first catch will log error
     }
+  }
 
-    /**
-     * @returns {String} The name of the subcommand
-     */
-    getSubcommand(){
-        return this.#interaction.options.getSubcommand();
+  /**
+   * @param {String} choiceName
+   */
+  async choiceNotExistsError(commandName, choiceName) {
+    this.#logger.logMessage(
+      LOG_SEVERITY.WARN,
+      commandName,
+      "N/A",
+      `ERROR: Requested choice: ${choiceName} does not exist for command: ${commandName}`
+    );
+    try {
+      await this.#interaction.reply({
+        content: `The specified choice '${choiceName}' does not exist`,
+        ephemeral: true,
+      });
+    } catch {
+      // If the interaction token has expired sending an error reply will cause a fatal error
+      // No need to log this as the first catch will log error
     }
-
-    /**
-     * @param {String} key The Name of the command option 
-     * @returns {?String} The value of the option if exists
-     */
-    getStringChoice(key){
-        return this.#interaction.options.getString(key);
-    }
-
-
-    /**
-     * @param {Object} error 
-     */
-    async genericError(functionName, error){
-        this.#logger.logMessage(LOG_SEVERITY.ERROR, functionName, error.stack, error.message);
-        try{
-            await this.#interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
-        }
-        catch{
-            // If the interaction token has expired sending an error reply will cause a fatal error
-            // No need to log this as the first catch will log error
-        }
-    }
-
-    /**
-     * @param {String} commandName 
-     */
-    async commandNotExistsError(commandName){
-        this.#logger.logMessage(LOG_SEVERITY.WARN, commandName, 'N/A', `ERROR: Requested command: ${commandName} does not exist`);
-        try{
-            await this.#interaction.reply({ content: `The specified command '${commandName}' does not exist`, ephemeral: true })
-        }
-        catch{
-            // If the interaction token has expired sending an error reply will cause a fatal error
-            // No need to log this as the first catch will log error
-        }
-    }
-
-    /**
-     * @param {String} choiceName 
-     */
-    async choiceNotExistsError(commandName, choiceName){
-        this.#logger.logMessage(LOG_SEVERITY.WARN, commandName, 'N/A', `ERROR: Requested choice: ${choiceName} does not exist for command: ${commandName}`);
-        try{
-            await this.#interaction.reply({ content: `The specified choice '${choiceName}' does not exist`, ephemeral: true })
-        }
-        catch{
-            // If the interaction token has expired sending an error reply will cause a fatal error
-            // No need to log this as the first catch will log error
-        }
-    }
+  }
 }
 
 module.exports = {
-    DiscordInteractionHandler
-}
+  DiscordInteractionHandler,
+};
--- powdermonkey/src/utils/embedBuilder.js
+++ powdermonkey/src/utils/embedBuilder.js
@@ -1,8 +1,13 @@
-const { MessageAttachment, MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
-const path = require('path');
+const {
+  MessageAttachment,
+  MessageEmbed,
+  MessageActionRow,
+  MessageButton,
+} = require("discord.js");
+const path = require("path");
 
-const embedColour = '#040728';
-const secondaryColour = '#025BEE';
+const embedColour = "#040728";
+const secondaryColour = "#025BEE";
 
 /**
  * @typedef {Object} Embed
@@ -13,166 +18,200 @@ const secondaryColour = '#025BEE';
  * @property {boolean?} ephemeral
  */
 
-class EmbedBuilder{
+class EmbedBuilder {
+  /**
+   * Adds a simple note to an existing embed
+   * @param {Embed} embed
+   * @param {string} note
+   * @returns {Embed}
+   */
+  addNote(embed, note) {
+    embed.embeds.push(
+      new MessageEmbed().setTitle(note).setColor(secondaryColour)
+    );
+    return embed;
+  }
 
-    /**
-     * Adds a simple note to an existing embed
-     * @param {Embed} embed
-     * @param {string} note
-     * @returns {Embed} 
-     */
-    addNote(embed, note){
-        embed.embeds.push(
-            new MessageEmbed()
-            .setTitle(note)
-            .setColor(secondaryColour)
-        );
-        return embed;
+  /**
+   *
+   * @param {string} title
+   * @param {string} message
+   * @param {string?} thumbnail
+   * @param {boolean} isEphemeral
+   * @returns {Embed}
+   */
+  createMessageEmbed(title, message, thumbnail, isEphemeral = true) {
+    const embed = new MessageEmbed()
+      .setTitle(title)
+      .setDescription(message)
+      .setColor(secondaryColour);
+    let file = null;
+
+    if (thumbnail) {
+      const filePath = path.join("src/assets/logos/", thumbnail);
+      file = new MessageAttachment(filePath);
+
+      embed.setThumbnail(`attachment://${thumbnail}`);
     }
 
-    /**
-     * @param {string} title Title of the embed
-     * @param {string} description Description of the embed
-     * @param {string} thumbnail filename of the thumbnail in the /assets/logos/ directory
-     * @param {string} url Url of the parent page
-     * @param {Object} fields optional fields to add
-     * @returns {Embed}
-     */
-    createSingleSubjectEmbed(title, description, thumbnail, url, fields, inline=true) {
+    return {
+      embeds: [embed],
+      files: file ? [file] : null,
+      ephemeral: isEphemeral,
+    };
+  }
 
-        const filePath = path.join('src/assets/logos/',thumbnail);
-        const file = new MessageAttachment(filePath);
+  /**
+   * @param {string} title Title of the embed
+   * @param {string} description Description of the embed
+   * @param {string} thumbnail filename of the thumbnail in the /assets/logos/ directory
+   * @param {string} url Url of the parent page
+   * @param {Object} fields optional fields to add
+   * @returns {Embed}
+   */
+  createSingleSubjectEmbed(
+    title,
+    description,
+    thumbnail,
+    url,
+    fields,
+    inline = true
+  ) {
+    const filePath = path.join("src/assets/logos/", thumbnail);
+    const file = new MessageAttachment(filePath);
 
-        const actionRow = new MessageActionRow()
-            .addComponents(new MessageButton()
-                .setLabel('Link')
-                .setStyle('LINK')
-                .setURL(url));
-        const embed = new MessageEmbed()
-            .setTitle(title)
-            .setThumbnail(`attachment://${thumbnail}`)
-            .setDescription(description)
-            .setColor(embedColour)
-            .setURL(url);
-        if(fields){
-            Object.keys(fields).forEach(key => 
-                embed.addField(key, fields[key],inline));
-        }
-
-        return{ 
-            embeds: [embed],
-            components: [actionRow],
-            files: [file],
-            ephemeral: true
-        }
+    const actionRow = new MessageActionRow().addComponents(
+      new MessageButton().setLabel("Link").setStyle("LINK").setURL(url)
+    );
+    const embed = new MessageEmbed()
+      .setTitle(title)
+      .setThumbnail(`attachment://${thumbnail}`)
+      .setDescription(description)
+      .setColor(embedColour)
+      .setURL(url);
+    if (fields) {
+      Object.keys(fields).forEach((key) =>
+        embed.addField(key, fields[key], inline)
+      );
     }
 
-    /**
-     * 
-     * @param {string} title 
-     * @param {string} description 
-     * @param {string} thumbnail 
-     * @param {Object} fields 
-     * @param {import('../commands/products').ActionProps[]} actions product price fetching service
-     * @returns {Embed} 
-     */
-    createMultiActionEmbed(title,description,thumbnail, fields, actions){
-        const filePath = path.join('src/assets/logos/',thumbnail);
-        const file = new MessageAttachment(filePath);
+    return {
+      embeds: [embed],
+      components: [actionRow],
+      files: [file],
+      ephemeral: true,
+    };
+  }
 
-        const actionRow = new MessageActionRow();
-        actions.forEach(action =>{
-            actionRow.addComponents(new MessageButton()
-                .setLabel(action.label)
-                .setStyle('LINK')
-                .setURL(action.url));
-        });
+  /**
+   *
+   * @param {string} title
+   * @param {string} description
+   * @param {string} thumbnail
+   * @param {Object} fields
+   * @param {import('../commands/products').ActionProps[]} actions product price fetching service
+   * @returns {Embed}
+   */
+  createMultiActionEmbed(title, description, thumbnail, fields, actions) {
+    const filePath = path.join("src/assets/logos/", thumbnail);
+    const file = new MessageAttachment(filePath);
 
-        const embed = new MessageEmbed()
-            .setTitle(title)
-            .setThumbnail(`attachment://${thumbnail}`)
-            .setDescription(description)
-            .setColor(embedColour)
-        if(fields){
-            Object.keys(fields).forEach(key => 
-                embed.addField(key, fields[key], true));
-        }
+    const actionRow = new MessageActionRow();
+    actions.forEach((action) => {
+      actionRow.addComponents(
+        new MessageButton()
+          .setLabel(action.label)
+          .setStyle("LINK")
+          .setURL(action.url)
+      );
+    });
 
-        return{ 
-            embeds: [embed],
-            components: [actionRow],
-            files: [file],
-            ephemeral: true
-        }
+    const embed = new MessageEmbed()
+      .setTitle(title)
+      .setThumbnail(`attachment://${thumbnail}`)
+      .setDescription(description)
+      .setColor(embedColour);
+    if (fields) {
+      Object.keys(fields).forEach((key) =>
+        embed.addField(key, fields[key], true)
+      );
     }
 
-    /**
-     * @param {string} title Title of the embed
-     * @param {string} description Description of the embed
-     * @param {string} thumbnail filename of the thumbnail in the /assets/logos/ directory
-     * @param {Object} fields Fields to be added
-     * @returns {Embed}
-     */
-    createMultiSubjectEmbed(title, description, thumbnail, fields){
-        const filePath = path.join('src/assets/logos/',thumbnail);
-        const file = new MessageAttachment(filePath);
+    return {
+      embeds: [embed],
+      components: [actionRow],
+      files: [file],
+      ephemeral: true,
+    };
+  }
 
-        const embed = new MessageEmbed()
-        .setTitle(title)
-        .setDescription(description)
-        .setColor(embedColour)
-        .setThumbnail(`attachment://${thumbnail}`);
-        Object.keys(fields).forEach(key =>
-            embed.addField(fields[key].name, fields[key].description));
+  /**
+   * @param {string} title Title of the embed
+   * @param {string} description Description of the embed
+   * @param {string} thumbnail filename of the thumbnail in the /assets/logos/ directory
+   * @param {Object} fields Fields to be added
+   * @returns {Embed}
+   */
+  createMultiSubjectEmbed(title, description, thumbnail, fields) {
+    const filePath = path.join("src/assets/logos/", thumbnail);
+    const file = new MessageAttachment(filePath);
 
-            return {
-                embeds: [embed],
-                files: [file],
-                ephemeral: true
-            };
-    }
+    const embed = new MessageEmbed()
+      .setTitle(title)
+      .setDescription(description)
+      .setColor(embedColour)
+      .setThumbnail(`attachment://${thumbnail}`);
+    Object.keys(fields).forEach((key) =>
+      embed.addField(fields[key].name, fields[key].description)
+    );
 
-    /**
-     * @param {string} title Title of the embed
-     * @param {string} description Comments to add with the embed
-     * @param {string} thumbnail filename of the thumbnail in the /assets/logos/ directory
-     * @param {string} mediaEmbed escaped html string for an embedded media player
-     * @param {Object} links Links to alternative sources
-     * @returns {Embed}
-     */
-    createMediaEmbed(title, description, thumbnail, mediaEmbed, links){
-        const filePath = path.join('src/assets/logos/',thumbnail);
-        const file = new MessageAttachment(filePath);
+    return {
+      embeds: [embed],
+      files: [file],
+      ephemeral: true,
+    };
+  }
 
-        var buttons = [];
-        Object.keys(links).forEach(key =>
-            buttons.push( new MessageButton()
-                .setLabel(`Listen on ${key}`)
-                .setStyle('LINK')
-                .setURL(links[key]))
-        );
-        
-        const embed = new MessageEmbed()
-                .setTitle(title)
-                .setDescription(description.toString()) //safety net for unescaped characters
-                .setColor(embedColour)
-                .setThumbnail(`attachment://${thumbnail}`); 
+  /**
+   * @param {string} title Title of the embed
+   * @param {string} description Comments to add with the embed
+   * @param {string} thumbnail filename of the thumbnail in the /assets/logos/ directory
+   * @param {string} mediaEmbed escaped html string for an embedded media player
+   * @param {Object} links Links to alternative sources
+   * @returns {Embed}
+   */
+  createMediaEmbed(title, description, thumbnail, mediaEmbed, links) {
+    const filePath = path.join("src/assets/logos/", thumbnail);
+    const file = new MessageAttachment(filePath);
 
-        const actionRow = new MessageActionRow()
-                .addComponents(buttons);
+    var buttons = [];
+    Object.keys(links).forEach((key) =>
+      buttons.push(
+        new MessageButton()
+          .setLabel(`Listen on ${key}`)
+          .setStyle("LINK")
+          .setURL(links[key])
+      )
+    );
 
-        return {
-            content: "[\u200B](" + mediaEmbed + ")",
-            components: [actionRow],
-            embeds: [embed],
-            files: [file],
-            ephemeral: true
-        };
-    }
+    const embed = new MessageEmbed()
+      .setTitle(title)
+      .setDescription(description.toString()) //safety net for unescaped characters
+      .setColor(embedColour)
+      .setThumbnail(`attachment://${thumbnail}`);
 
+    const actionRow = new MessageActionRow().addComponents(buttons);
 
+    return {
+      content: "[\u200B](" + mediaEmbed + ")",
+      components: [actionRow],
+      embeds: [embed],
+      files: [file],
+      ephemeral: true,
+    };
+  }
 }
 
 module.exports = {
-    EmbedBuilder
-}
+  EmbedBuilder,
+};

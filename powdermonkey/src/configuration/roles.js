db545ac6d81b00d7ac30188d585e878bbb6959be
crown_dev <crown@galleon.community>
2022-06-20 12:19:44 +0100

    fix boatswain naming

--- powdermonkey/src/configuration/roles.js
+++ powdermonkey/src/configuration/roles.js
@@ -1,7 +1,7 @@
 const ADMIN_ROLES = {
   "Core: Captain": "860441754694844427",
   "Core: Crew": "931209523983683615",
-  "Crew:Boatswain": "959522797896486993",
+  "Crew: Boatswain": "959522797896486993",
 };
 
 const DEBUG_ADMIN_ROLES = {

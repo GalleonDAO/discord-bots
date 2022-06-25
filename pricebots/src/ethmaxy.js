d1703391f85d0c4af7cb1fd853b92b4f58a75e4a
crown_dev <crown@galleon.community>
2022-06-25 13:37:12 +0100

    fix ethmaxy

--- pricebots/src/ethmaxy.js
+++ pricebots/src/ethmaxy.js
@@ -2,14 +2,14 @@ const {
   fetchCoingeckoData,
   fetchTokensetsData,
   tokensetsIsValid,
-  fetchTokensetsChange
-} = require('./helpers/utils')
+  fetchTokensetsChange,
+} = require('./services/tokenDataService')
 const {
   getNickname,
   getActivity,
-  setDiscordText
+  setDiscordText,
 } = require('./helpers/discordhelpers')
-const Client = require('discord.js')
+const { Client } = require('discord.js')
 const dotenv = require('dotenv')
 
 const COINGECKO_TOKENID = 'eth-max-yield-index'
@@ -17,7 +17,9 @@ const TOKENSETS_TOKENID = 'ethmaxy'
 
 dotenv.config()
 //Allow token to be supplied as DISCORD_API_TOKEN_ETHMAXY or DISCORD_API_TOKEN for ease of use
-const DISCORD_API_TOKEN = !process.env.DISCORD_API_TOKEN_ETHMAXY ? process.env.DISCORD_API_TOKEN : process.env.DISCORD_API_TOKEN_ETHMAXY
+const DISCORD_API_TOKEN = !process.env.DISCORD_API_TOKEN_ETHMAXY
+  ? process.env.DISCORD_API_TOKEN
+  : process.env.DISCORD_API_TOKEN_ETHMAXY
 
 let client = new Client()
 client.login(DISCORD_API_TOKEN)
@@ -38,32 +40,31 @@ const updateBot = async () => {
   const tokensetsData = await fetchTokensetsData(TOKENSETS_TOKENID)
 
   if (!coingeckoData || !tokensetsData) {
-    console.warn('Missing data\r\ncoingecko: ' + JSON.stringify(coingeckoData) + '\r\ntokensets: ' + JSON.stringify(tokensetsData))
+    console.warn(
+      'Missing data\r\ncoingecko: ' +
+        JSON.stringify(coingeckoData) +
+        '\r\ntokensets: ' +
+        JSON.stringify(tokensetsData),
+    )
     return
   }
 
   if (tokensetsIsValid(tokensetsData)) {
-    let {
-      change
-    } = coingeckoData
-    const {
-      price,
-      symbol,
-      marketCap
-    } = tokensetsData
+    let { change } = coingeckoData
+    const { price, symbol, marketCap } = tokensetsData
 
     console.log('Fetched: ' + symbol, price, marketCap)
 
-    if(!change){
-      console.log('No Price change available for Coingecko, using fallback');
-      change = await fetchTokensetsChange(TOKENSETS_TOKENID);
-      if(!change)
-        console.log('No Price change available for Tokensets, failing over');
+    if (!change) {
+      console.log('No Price change available for Coingecko, using fallback')
+      change = await fetchTokensetsChange(TOKENSETS_TOKENID)
+      if (!change)
+        console.log('No Price change available for Tokensets, failing over')
     }
 
-    const nickname = getNickname(price, change);
-    const activity = getActivity(marketCap);
-    
-    setDiscordText(client, nickname, activity);
+    const nickname = getNickname(price, change)
+    const activity = getActivity(marketCap)
+
+    setDiscordText(client, nickname, activity)
   }
 }
